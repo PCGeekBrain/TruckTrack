9dca92da057e9d119a10b970cdf99e87ca8bca6b
Menachem Hornbacher <mendelh1537@gmail.com>
2017-08-01 13:09:27 -0400

    added decoding

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -3,8 +3,19 @@ require 'jwt'
 class JsonWebToken
 
   def self.encode(payload, exp = 7.days.from_now)
+    # add the exparation to the payload
     payload[:exp] = exp.to_i
+    # encode it with the secret from the configurations
     jwt.encode(payload, Rails.application.secrets.secret_key_base)
   end
-  
+
+  def self.decode(token)
+    # decode the token with the same key from the configuration
+    body = JWT.decode(token, Rails.application.secrets.secret_key_base)
+    # Return the body with constants working as keys instead of just strings
+    return HashWithIndifferentAccess.new(body)
+  # if the decoding has an error reutrn nil
+  rescue
+    return nil
+  end
 end
