d72fea127b34ce9861a4a5236373a91515d45381
Menachem Hornbacher <mendelh1537@gmail.com>
2017-08-01 13:40:22 -0400

    added some docs

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -1,15 +1,47 @@
 require 'jwt'
 
+############################################
+# Class JsonWebToken
 # Custom JWT handler for API authentication
+#
+# This class folows the singleton pattern and
+# only has class functions
+# 
+# Functions:
+#
+# - JsonWebToken.encode(payload, exparation (optional))
+#   returns JWT encoded with Rails secret_key_base
+#
+# - JsonWebToken.decode(token)
+#   decodes payload from token or nil if invalid
+#
+# - JsonWebToken.valid_payload(payload)
+#   checks if the payload hash is valid
+#
+# - JsonWebToken.expired(payload)
+#   checks if the payload hash has expired
+#
+############################################
 
 class JsonWebToken
+
+  # JsonWebToken.encode(payload, exp)
+  # encodes the payload with a default 7 day exparation
+
+  # Example: JsonWebToken.encode({foo: "bar"}) #=> "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJmb28iOiJiYXIiLCJleHAiOjE1MDIyMTM5ODh9.BosnwiTQ3pMMN7LB_D8GJlvHUYTNIOqLsaT6V1_Rnxs"
 
   def self.encode(payload, exp = 7.days.from_now)
     # add the exparation to the payload
     payload[:exp] = exp.to_i
     # encode it with the secret from the configurations
-    jwt.encode(payload, Rails.application.secrets.secret_key_base)
+    JWT.encode(payload, Rails.application.secrets.secret_key_base)
   end
+
+  # JsonWebToken.decode(token)
+  # decodes the token and returns the payload
+  # returns nil if invalid token
+
+  # Example: JsonWebToken.decode("") #=> nil
 
   def self.decode(token)
     # decode the token with the same key from the configuration
@@ -21,16 +53,28 @@ class JsonWebToken
     return nil
   end
 
+  # JsonWebToken.valid_payload(payload)
+  # check if the provided payload is a valid JWT payload.
+  # checks the following things:
+  #   - exparation
+
+  # Example: JsonWebToken.valid_payload({exp: 1.day.ago}) #=> false
+
   def self.valid_payload(payload)
     # TODO add meta checks as well if needed.
-    if expired(payload)
+    # if it is invalid return false
+    if expired(payload) 
       return false
     else
       return true
     end
   end
 
-  # check if the token is expired
+  # JsonWebToken.expired(payload)
+  # check if the payload is expired
+
+  # Example: JsonWebToken.expired({exp: 1.day.from_now}) #> false
+
   def self.expired(payload)
     Time.at(payload['exp']) < Time.now
   end
