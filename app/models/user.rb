d087034194925b36eed80b3a0f1bb3a1fcaf3ca5
Menachem Hornbacher <mendelh1537@gmail.com>
2017-08-01 16:15:03 -0400

    Added user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,35 @@
 class User < ApplicationRecord
+  # encrypt the password and check for varification
   has_secure_password
 
-  enum role: {user: 0, agent: 10, driver: 20, manager: 30, admin: 40}
+  # Authorization roles
+  enum role: {
+    user: 0, 
+    agent: 10, 
+    driver: 20, 
+    manager: 30, 
+    admin: 40
+  }
+
+  # Validations
+
+  # username:
+  # - Must be greater then 8 characters
+  # - Must be present
+  # - Must be unique
+  validates :username, presence: true, uniqueness: true, length: {minimum: 8}
+
+  # password:
+  # - Must be between 8 and 40 characters
+  validates :password, length: {within: 8..40}
+
+  # password_confirmation:
+  # - Must be present when user is created
+  validates :password_confirmation, presence: true, on: :create
+
+  # email:
+  # - Must be present
+  # - Must be valid (see app/validators/email_validator)
+  validates :email, presence: true, email: true
+  
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,64 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+RSpec.describe User, :type => :model do
+  
+    describe "Fields:" do
+  
+      it "has a username" do
+        expect(User.new).to respond_to(:username)
+      end
+  
+      it "has a passowrd_digest" do
+        expect(User.new).to respond_to(:password_digest)
+      end
+  
+      it "has a email" do
+        expect(User.new).to respond_to(:email)
+      end
+      
+    end
+  
+    describe "Validations:" do
+  
+      before(:each) do
+        @valid_user_hash = {username: "BobMarly24", 
+                            password: "Password", 
+                            email: "test@test.com",
+                            password_confirmation: "Password"
+                          }
+        @valid_user = User.create(@valid_user_hash)
+      end
+  
+      it "username is present" do
+        user = User.new(password: "password")
+        expect(user.valid?).to be(false)
+        expect(user.errors).to include(:username)
+      end
+  
+      it "username is unique" do
+        user = User.new(username: "BobMarly24", password: "Passoword", email: "test@test.com")
+        expect(user.valid?).to be(false)
+        expect(user.errors).to include(:username)
+      end
+  
+      it "email is valid" do
+        user = User.new(email: "test.com")
+        expect(user.valid?).to be(false)
+        expect(user.errors).to include(:email)
+      end
+  
+      it "password is longer then 8 characters" do
+        user = User.new(password: "test")
+        expect(user.valid?).to be(false)
+        expect(user.errors).to include(:password)
+      end
+  
+      it "hashes the password" do
+        user = User.create(username: "localhost", password: "password", email: "test@test.com")
+        expect(user.password_digest).to_not be(nil);
+        expect(user.password_digest).to_not be("password");
+        expect(user.password_digest.length).to eq(60) # subject to change but should break when encryption changes
+      end
+  
+    end
+  end
