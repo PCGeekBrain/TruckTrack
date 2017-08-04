39d1491fe84ea2dc7c6b780cdc6c32e96200f93d
Menachem Hornbacher <mendelh1537@gmail.com>
2017-08-04 12:19:51 -0400

    Added routes

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,4 +1,22 @@
 class LoginController < ApplicationController
+
   def login
+    # find the user in the database (nil if the username is false)
+    user = User.find_by(username: params[:username])
+    # if the user is nil the username is invalid and the first condition will fail
+    if user && user.authenticate(params[:password])
+      # if the user is logged in return a useable JWT token
+      token = JsonWebToken.encode({id: user.id, username: user.username, email: user.email})
+      render json: {
+        message: "Login Successful",
+        token: token
+      }, status: :ok
+    else
+      render json: {
+        message: "Username and password are incorrect",
+      }, status: :unauthorized
+    end
+
   end
+
 end
