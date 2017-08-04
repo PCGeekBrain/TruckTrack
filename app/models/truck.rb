fb47bfb573bb59b262d07b4c2918fb2e7557e549
Menachem Hornbacher <mendelh1537@gmail.com>
2017-08-04 00:16:11 -0400

    added has many deliveries relationship

--- app/models/truck.rb
+++ app/models/truck.rb
@@ -1,9 +1,14 @@
 class Truck < ApplicationRecord
-  has_many :routes
 
+  # Relationships
+  has_many :routes
+  has_many :deliveries, through: :routes
   has_many :drivers, through: :routes
 
+  # Validations
+  
+  # Name must be present and unique
   validates :name, presence: true, uniqueness: true
-
+  # licence plates must be unique
   validates :licence, uniqueness: true, allow_nil: true
 end
