e8eb518ca8db1875768d680aae290a8ae909a7e8
Menachem Hornbacher <mendelh1537@gmail.com>
2017-08-04 00:16:59 -0400

    added deliverie relationship with users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # has many routes (for drivers)
   has_many :routes
-
+  has_many :deliveries, through: :routes
   has_many :trucks, through: :routes
 
   # encrypt the password and check for varification
