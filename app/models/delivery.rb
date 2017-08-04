db9941d2675dbef2ad1ea93286ac7293f20a71a4
Menachem Hornbacher <mendelh1537@gmail.com>
2017-08-03 23:46:52 -0400

    Added validations and hooks

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -1,3 +1,23 @@
 class Delivery < ApplicationRecord
+  # Hooks
+  # update the delivered_at if delivered is set to true
+  before_update :update_delivered_at, if: :delivered_changed?
+  
+  # Relationships
   belongs_to :route
+  has_one :truck, through: :route
+
+  # Validations
+  validates :invoice_number, presence: true
+
+  private
+  ############################################
+  # Hook functions
+  ############################################
+  # #update_delivered_at => sets delivered at to current time if item was delivered.
+  def update_delivered_at
+    if self.delivered
+      self.delivered_at = Time.now
+    end
+  end
 end
