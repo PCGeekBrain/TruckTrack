67e5b1f10e5403352a8f59f8f3022c988f66a585
Menachem Hornbacher <mendelh1537@gmail.com>
2017-08-03 20:27:19 -0400

    Added route (and tests)

---
+++ app/models/route.rb
@@ -0,0 +1,15 @@
+class Route < ApplicationRecord
+  belongs_to :driver, class_name: "User", foreign_key: "user_id"
+
+  enum status: {
+    created: 0,
+    loading: 10,
+    loaded: 20,
+    departed: 30,
+    en_route: 40,
+    returning: 50,
+    compleated: 60
+  }
+
+  validates :log_number, presence: true
+end
---
+++ spec/models/route_spec.rb
@@ -0,0 +1,53 @@
+require 'rails_helper'
+
+RSpec.describe Route, type: :model do
+
+  describe "Fields:" do
+
+    let(:route){Route.new}
+
+    it "has a log_number" do
+      expect(route).to respond_to(:log_number)
+    end
+    
+    it "has a enum status that defaults to 0" do
+      expect(route).to respond_to(:status)
+      expect(Route.statuses[route.status]).to eq(0)
+    end
+  
+  end
+
+  describe "Relationships:" do
+
+    before(:each) do
+      @user = User.create(username: "localhost", password: "password", password_confirmation: "password")
+      @route = Route.new
+    end
+
+    it "belongs to a user, aliased as driver" do
+      expect(@route.driver).to eq(nil)
+      expect(@route.user_id).to eq(nil)
+      @route.driver = @user
+      expect(@route.driver).to eq(@user)
+      expect(@route.user_id).to eq(@user.id)
+    end
+
+    it "belongs to a truck"
+
+    it "has many deliveries"
+
+    it "has many comments through deliveries"
+
+  end
+
+  describe "Validations:" do
+    
+    it "requires presence for log_number" do
+      route = Route.new
+      route.valid?
+      expect(route.errors).to include(:log_number)
+    end
+
+  end
+
+end
