8dc63fa6aa7edb8725dbd8f4a2d8065fff0dd300
Menachem Hornbacher <mendelh1537@gmail.com>
2017-08-04 19:20:42 -0400

    added json responses for bad authorization calls

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,21 @@
 class ApplicationController < ActionController::API
+  # Incluce the authenticaion concern
   include Authentication
+
+  # Hadle errors with the correct JSON responses of.
+  rescue_from AuthenticationError::TimeoutError, with: :authentication_timeout
+  rescue_from AuthenticationError::AccessDeniedError, with: :forbidden_resource
+  rescue_from AuthenticationError::NotAuthenticatedError, with: :user_not_authenticated
+
+  def authentication_timeout
+    render json: { errors: ['Authentication Timeout'] }, status: 419
+  end
+
+  def forbidden_resource
+    render json: { errors: ['Not Authorized To Access Resource'] }, status: :forbidden
+  end
+
+  def user_not_authenticated
+    render json: { errors: ['Not Authenticated'] }, status: :unauthorized
+  end
 end
