18085b3bd203e598296ecb40b30f6f3f85764b06
Menachem Hornbacher <mendelh1537@gmail.com>
2017-08-06 15:27:49 -0400

    setup app inside store with provider

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,20 @@
+// React
 import React from 'react';
 import ReactDOM from 'react-dom';
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
+// Components
+import App from './components/App';
+// Styles
 import './index.css';
-import App from './App';
+// Other
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, 
+  document.getElementById('root')
+);
 registerServiceWorker();
