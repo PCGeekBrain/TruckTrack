73aecc3ca4c33a117010a576edab1d82c58c715c
Menachem Hornbacher <mendelh1537@gmail.com>
2017-08-06 19:43:10 -0400

    pass the required props to SearchBar (and add the css for fun :-) )

--- client/src/containers/HomeContainer.js
+++ client/src/containers/HomeContainer.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 // Components
-import Delivery from '../components/home/Delivery'
-import Searchbar from '../components/home/Searchbar'
-import {} from 'react-bootstrap';
+import Delivery from '../components/home/Delivery';
+import Searchbar from '../components/home/Searchbar';
+
+import { getResults } from '../actions/home'
 // Styles
+import '../styles/home/index.css'
 
 class HomeContainer extends Component {
   render(){
@@ -12,7 +14,7 @@ class HomeContainer extends Component {
     return (
       <div className="page-home container">
         <h1 id="title">Track Shipment</h1>
-        <Searchbar />
+        <Searchbar options={["invoice", "tracking"]} onSubmit={this.props.getResults}/>
         <ul id="deliveries">
           {deliveries}
         </ul>
@@ -26,4 +28,4 @@ function mapStateToProps(state, ownProps){
     deliveries: state.home
   }
 }
-export default connect(mapStateToProps)(HomeContainer)
+export default connect(mapStateToProps, {getResults})(HomeContainer)
