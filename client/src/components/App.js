970e99ffe64f541b6ad9ac733720b98ec771b391
Menachem Hornbacher <mendelh1537@gmail.com>
2017-08-06 18:29:19 -0400

    show the home container

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,19 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
+// Components
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import HomeContainer from '../containers/HomeContainer';
+// Styles
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <Route exact path="/" component={HomeContainer} />
+        {/* <Route path="/dashboard" component={Home} /> */}
       </div>
-    );
-  }
+    </Router>
+  );
 }
 
 export default App;
