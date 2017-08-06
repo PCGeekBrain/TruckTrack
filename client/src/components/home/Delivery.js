403cd418c59f6ec7840591ecc9f3a8a60e078fa0
Menachem Hornbacher <mendelh1537@gmail.com>
2017-08-06 19:42:17 -0400

    Added exporting

--- client/src/components/home/Delivery.js
+++ client/src/components/home/Delivery.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import '../../styles/home/Delivery.css'
 
 const Delivery = ({tracking_number, status}) => {
   return (
-    <div className="delivery">
+    <div className="delivery-card">
       <h3>{tracking_number}</h3>
       <p>{status}</p>
     </div>
   );
 }
+
+export default Delivery;
