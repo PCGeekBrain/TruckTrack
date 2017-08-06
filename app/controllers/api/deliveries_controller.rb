b074e114cfd4b01278d6c9945b04eeb8f54510d2
Menachem Hornbacher <mendelh1537@gmail.com>
2017-08-06 13:22:10 -0400

    added CRUD

--- app/controllers/api/deliveries_controller.rb
+++ app/controllers/api/deliveries_controller.rb
@@ -1,17 +1,60 @@
 class Api::DeliveriesController < ApplicationController
+  # authenticate the user, get the route and validate user role before CRUD operations
+  before_action :authenticate_request!, only: [:index, :show, :create, :update, :destroy]
+  before_action :load_route, only: [:index, :show, :create, :update, :destroy]
+  before_action :validate_driver, only: :update
+  before_action :validate_manager, only: [:create, :destroy]
 
   def index
+    render json: @route.deliveries
   end
 
   def show
+    render json: @route.deliveries.find(params[:id])
   end
 
   def create
+    delivery = @route.deliveries.build(post_params)
+    if delivery.save
+      render json: delivery, status: :created
+    else
+      render json: {errors: delivery.errors}, status: :bad_request
+    end
   end
   
   def update
+    delivery = @route.deliveries.find(params[:id])
+    if delivery.update(post_params)
+      render json: delivery, status: :accepted
+    else
+      render json: {errors: delivery.errors}, status: :bad_request
+    end
   end
 
   def destroy
+    delivery = @route.deliveries.find(params[:id])
+    if delivery.destroy
+      head :no_content
+    else
+      render json: {error: "Could not delete delivery"}
+    end
+  end
+
+  private
+
+  def load_route
+    @route = Route.find(params[:route_id])
+  end
+
+  def validate_driver
+    raise AuthenticationError::AccessDeniedError unless current_user.fits_role?("driver")
+  end
+
+  def validate_manager
+    raise AuthenticationError::AccessDeniedError unless current_user.fits_role?("manager")
+  end
+
+  def post_params
+    params.require(:delivery).permit(:invoice_number, :cod, :address, :phone_number, :delivered)
   end
 end
--- spec/controllers/api/deliveries_controller_spec.rb
+++ spec/controllers/api/deliveries_controller_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe Api::DeliveriesController, type: :controller do
     @admin = User.create(role: "admin", username: "localhost", password: "password")
     @truck = Truck.create(name: "truck1")
     @route = Route.create(driver: @admin, truck: @truck, log_number: "test1234")
+    @delivery = @route.deliveries.create(invoice_number: "156795")
   end
 
   it "requires authentication" do
@@ -15,15 +16,82 @@ RSpec.describe Api::DeliveriesController, type: :controller do
 
   describe "GET #index" do
     before(:each) do
-      @user = User.create(username: "localhost", password: "password")
-      @driver = User.create(username: "driveruser", password: "password", role: "driver")
+      request.headers.merge('Authorization' => "token #{JsonWebToken.encode({id: @admin.id})}")
     end
-    it "returns a list of all deliveries"
 
-    it "only returns a list of driver deliveries if the user is a driver"
+    it "returns a list of all deliveries for the route" do
+      get :index, params: {route_id: @route.id}
+
+      expect(response.status).to eq(200)
+      expect(json.length).to eq(@route.deliveries.count)
+    end
+
+    it "returned list does not include deliveries for other routes" do
+      route2 = Route.create(driver: @admin, truck: @truck, log_number: "test5678")
+      route2.deliveries.create(invoice_number: "157002")
+      route2.deliveries.create(invoice_number: "157012")
+      route2.deliveries.create(invoice_number: "157032")
+
+      get :index, params: {route_id: route2.id}
+      
+      expect(response.status).to eq(200)
+      expect(json.length).to eq(route2.deliveries.count)
+      expect(json[0]["invoice_number"]).to eq("157002")
+    end
   end
 
-  describe "GET #show"
+  describe "GET #show" do
+    before(:each) do
+      request.headers.merge('Authorization' => "token #{JsonWebToken.encode({id: @admin.id})}")
+      get :show, params: {route_id: @route.id, id: @delivery.id}
+    end
+
+    it "should return http success" do
+      expect(response.status).to eq(200)
+    end
+
+    it "should include invoice_number" do
+      expect(json["invoice_number"]).to eq(@delivery.invoice_number)
+    end
+
+    it "should include delivered_at" do
+      expect(json["delivered_at"]).to eq(@delivery.delivered_at)
+    end
+
+    it "should include cod" do
+      expect(json["cod"]).to eq(@delivery.cod)
+    end
+
+    it "should include address" do
+      expect(json["address"]).to eq(@delivery.address)
+    end
+
+    it "should include phone_number" do
+      expect(json["phone_number"]).to eq(@delivery.phone_number)
+    end
+
+    it "should include delivered" do
+      expect(json["delivered"]).to eq(@delivery.delivered)
+    end
+
+    it "should include tracking_number" do
+      expect(json["tracking_number"]).to eq(@delivery.tracking_number)
+    end
+
+    # TODO test status
+    describe "status" do
+      it "returns the routes status" do
+        expect(json["status"]).to eq(@delivery.route.status)
+      end
+
+      it "returns 'delivered' if delivered is set to true" do
+        @delivery.update(delivered: true)
+        get :show, params: {route_id: @route.id, id: @delivery.id} # reload the page to get the updated status
+        
+        expect(json["status"]).to eq("delivered")
+      end
+    end
+  end
 
   describe "POST #create"
 
