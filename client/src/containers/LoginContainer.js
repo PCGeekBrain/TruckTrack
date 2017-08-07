88c7a8aac67971746554120fd15689df680fa2ca
Menachem Hornbacher <mendelh1537@gmail.com>
2017-08-07 01:04:12 -0400

    transfered to stateless component. Also redirect to dashboard if logged out

--- client/src/containers/LoginContainer.js
+++ client/src/containers/LoginContainer.js
@@ -1,25 +1,28 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {connect} from 'react-redux';
 // Components
 import Login from '../components/login/Login';
+import { Redirect } from 'react-router';
 // Actions
-import {login} from '../actions/login';
+import { login } from '../actions/login';
 // Styles
 
-class LoginContainer extends Component {
-  render() {
-    return (
-      <div className="page-login container">
-        <h1 id="title">Login</h1>
-        <Login error={this.props.error} onSubmit={this.props.login} />
-      </div>
-    )
+const LoginContainer = ({error, login, logged_in}) => {
+  if (logged_in){
+    return <Redirect to="/dashboard"/>
   }
+  return (
+    <div className="page-login container">
+      <h1 id="title">Login</h1>
+      <Login error={error} onSubmit={login} />
+    </div>
+  )
 }
 
 function mapStateToProps(state) {
   return {
-    error: state.login.error
+    error: state.login.error,
+    logged_in: state.login.logged_in
   }
 }
 
