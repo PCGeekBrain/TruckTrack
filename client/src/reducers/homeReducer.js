035d0aa0ddbadb06bd6ae8d6b2cff2c3ed2ac1d6
Menachem Hornbacher <mendelh1537@gmail.com>
2017-08-06 21:12:09 -0400

    Handle 404 errors where results is passed as an object not array

--- client/src/reducers/homeReducer.js
+++ client/src/reducers/homeReducer.js
@@ -1,7 +1,11 @@
 export default (state = [], action) => {
   switch (action.type) {
     case "HOME_NEW_RESULTS":
-      return action.results
+      if (Array.isArray(action.results)){
+        return action.results
+      } else {
+        return state
+      }
     default:
       return state
   }
