9b97b6670363a925e2822db1b08f99313d838372
Menachem Hornbacher <mendelh1537@gmail.com>
2017-08-11 16:31:22 -0400

    add routes to the root reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,8 +2,10 @@ import { combineReducers } from 'redux';
 
 import homeReducer from './homeReducer';
 import loginReducer from './loginReducer';
+import routeReducer from './routeReducer';
 
 export default combineReducers({
   home: homeReducer,
-  login: loginReducer
+  login: loginReducer,
+  routes: routeReducer
 })
