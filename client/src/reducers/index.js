428317cac5ea0385604a39d5b3e01aa39dc9820d
Menachem Hornbacher <mendelh1537@gmail.com>
2017-08-13 13:16:55 -0400

    add deliveries reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -3,9 +3,11 @@ import { combineReducers } from 'redux';
 import homeReducer from './homeReducer';
 import loginReducer from './loginReducer';
 import routeReducer from './routeReducer';
+import deliveryReducer from './deliveryReducer';
 
 export default combineReducers({
   home: homeReducer,
   login: loginReducer,
-  routes: routeReducer
+  routes: routeReducer,
+  deliveries: deliveryReducer
 })
