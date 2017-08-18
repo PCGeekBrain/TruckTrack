16ee3b81e61f258793f59ebf4f07bd05accd15d1
Menachem Hornbacher <mendelh1537@gmail.com>
2017-08-18 16:19:49 -0400

    leave inheritence from component

--- client/src/components/routes/RoutePage.js
+++ client/src/components/routes/RoutePage.js
@@ -6,33 +6,31 @@ import DeliveryContainer from '../../containers/DeliveryContainer';
 
 import '../../styles/routes/route.css';
 
-class RoutePage extends Component{
+const RoutePage = (props) => {
 
-  render () {
-    const last_updated = new Date(this.props.updated_at).toDateString();
-    const delivery_list = this.props.deliveries.map((delivery, index) => 
-      <DeliveryContainer key={index} route_id={this.props.id} delivery={delivery}/>
-    )
+  const last_updated = new Date(props.updated_at).toDateString();
+  const delivery_list = props.deliveries.map((delivery, index) => 
+    <DeliveryContainer key={index} route_id={props.id} delivery={delivery}/>
+  )
 
-    return (
-      <div id={`route-${this.props.id}`}>
-        <h1>Route -> Log number {this.props.log_number}</h1>
-        <ul>
-          <li>Status: {this.props.status}</li>
-          <li>Driver: {this.props.driver.username}</li>
-          <li>Truck: {this.props.truck.name}</li>
-          <li>Last Updated: {last_updated}</li>
-        </ul>
-        <h2>
-          Deliveries:
-          <button className="btn btn-primary" onClick={this.props.createDelivery}>Add Delivery</button>
-        </h2>
-        <div className="deliveries">
-          {delivery_list}
-        </div>
+  return (
+    <div id={`route-${props.id}`}>
+      <h1>Route -> Log number {props.log_number}</h1>
+      <ul>
+        <li>Status: {props.status}</li>
+        <li>Driver: {props.driver.username}</li>
+        <li>Truck: {props.truck.name}</li>
+        <li>Last Updated: {last_updated}</li>
+      </ul>
+      <h2>
+        Deliveries:
+        <button className="btn btn-primary" onClick={props.createDelivery}>Add Delivery</button>
+      </h2>
+      <div className="deliveries">
+        {delivery_list}
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default RoutePage;
