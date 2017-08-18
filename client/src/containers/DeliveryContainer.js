ecb351f619e4ff906ec8128f0b39db538ffee122
Menachem Hornbacher <mendelh1537@gmail.com>
2017-08-18 18:36:39 -0400

    add editing powers

--- client/src/containers/DeliveryContainer.js
+++ client/src/containers/DeliveryContainer.js
@@ -2,19 +2,24 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import Delivery from '../components/deliveries/Delivery';
-import { markDelivered } from '../actions/delivery'
+import { markDelivered, setShowModal, setActiveDelivery } from '../actions/delivery'
 
 const DeliveryContainer = (props) => {
 
   const onDelivered = () => {
     props.markDelivered(props.route_id, props.delivery.id)
   }
-  
+
+  const editDelivery = () => {
+    props.setActiveDelivery(props.delivery);
+    props.setShowModal(true);
+  }
+
   return (
     <div className="card card-delivery container-delivery">
-      <Delivery {...props.delivery} onDelivered={onDelivered} />
+      <Delivery {...props.delivery} onDelivered={onDelivered} onEdit={editDelivery} />
     </div>
   )
 }
 
-export default connect(null, { markDelivered })(DeliveryContainer)
+export default connect(null, { markDelivered, setShowModal, setActiveDelivery })(DeliveryContainer)
