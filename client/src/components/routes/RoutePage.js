e88fa29fa549dec087e0a0999cfdaa6d681512cb
Menachem Hornbacher <mendelh1537@gmail.com>
2017-08-18 16:11:13 -0400

    add modal and move to inherit from component

--- client/src/components/routes/RoutePage.js
+++ client/src/components/routes/RoutePage.js
@@ -1,28 +1,38 @@
-import React from 'react';
+import React, {Component} from 'react';
+
+import { Modal } from 'react-bootstrap';
+
+import DeliveryContainer from '../../containers/DeliveryContainer';
 
 import '../../styles/routes/route.css';
 
-const RoutePage = ({
-  id, log_number, status, delivery_count, 
-  updated_at, driver, truck, deliveries
-}) => {
-  const last_updated = new Date(updated_at).toDateString();
-  const delivery_list = deliveries.map(delivery => <p>{delivery.invoice_number}</p>)
-  return (
-    <div id={`route-${id}`}>
-      <h1>Route -> Log number {log_number}</h1>
-      <ul>
-        <li>Status: {status}</li>
-        <li>Driver: {driver.username}</li>
-        <li>Truck: {truck.name}</li>
-        <li>Last Updated: {last_updated}</li>
-      </ul>
-      <h2>Deliveries:</h2>
-      <div className="deliveries">
-        {delivery_list}
+class RoutePage extends Component{
+
+  render () {
+    const last_updated = new Date(this.props.updated_at).toDateString();
+    const delivery_list = this.props.deliveries.map((delivery, index) => 
+      <DeliveryContainer key={index} route_id={this.props.id} delivery={delivery}/>
+    )
+
+    return (
+      <div id={`route-${this.props.id}`}>
+        <h1>Route -> Log number {this.props.log_number}</h1>
+        <ul>
+          <li>Status: {this.props.status}</li>
+          <li>Driver: {this.props.driver.username}</li>
+          <li>Truck: {this.props.truck.name}</li>
+          <li>Last Updated: {last_updated}</li>
+        </ul>
+        <h2>
+          Deliveries:
+          <button className="btn btn-primary" onClick={this.props.createDelivery}>Add Delivery</button>
+        </h2>
+        <div className="deliveries">
+          {delivery_list}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default RoutePage;
