61dd10f1d324520e5215b1d09a18899abd85b69a
Menachem Hornbacher <mendelh1537@gmail.com>
2017-08-18 16:12:57 -0400

    show the modal

--- client/src/containers/RouteContainer.js
+++ client/src/containers/RouteContainer.js
@@ -2,34 +2,71 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 // components
 import RoutePage from '../components/routes/RoutePage';
+import DeliveryModal from '../components/deliveries/DeliveryModal';
 
 import { getDeliveries } from '../actions/delivery';
-import { getRoutes } from '../actions/route';
+import { getRoute } from '../actions/route';
 
 class RouteContainer extends Component {
   componentWillMount(){
-    this.props.getRoutes()
+    // get the route from the api
+    this.props.getRoute(this.props.id)
+    // get the deliveries from the api
+    this.props.getDeliveries(this.props.id)
+    this.state = { showModal: false, delivery: undefined }
+  }
+
+  // onEditDelivery = () => {
+  //   console.log("TODO post data to server")
+  //   // get all updates to the list (even from others)
+  //   this.props.getDeliveries(this.props.id)
+  // }
+
+  showModal = (delivery_id) => {
+    const delivery = this.props.deliveries.filter(delivery => delivery.id === delivery_id)
+    this.setState({
+      showModal: true,
+      delivery: delivery ? delivery : {}
+    })
+  }
+
+  hideModal = () => {
+    this.setState({showModal: false})
+  }
+
+  onCreateDelivery = (delivery) => {
+    // todo, post delivery to server
+    console.log("TODO: post update to server", delivery)
+    this.hideModal()
+    // get all updates to the list (even from others)
     this.props.getDeliveries(this.props.id)
   }
+
   render() {
     return (
       <div id="page-route">
         {this.props.route ? 
-        <RoutePage {...this.props.route} deliveries={this.props.deliveries} /> :
-        <h1>404: Route not found</h1>}
+        <RoutePage {...this.props.route} 
+            deliveries={this.props.deliveries} 
+            editDelivery={this.editDelivery}
+            createDelivery={this.showModal} onCreateDelivery={this.onCreateDelivery} /> :
+        <h1>Error: Route not found</h1>}
+        {this.state.showModal &&
+          <DeliveryModal delivery={this.state.delivery} onSave={this.onCreateDelivery} onCancel={this.hideModal} />
+        }
       </div>
     )
   }
 }
 
 function mapStateToProps(state, ownProps) {
-  const id = parseInt(ownProps.match.params.id);
+  const id = parseInt(ownProps.match.params.id, 10); // cast the id to an int. makes life easy :-)
   return {
     match: ownProps.match,
     id: id,
-    route: state.routes.find(route => route.id === id),
+    route: state.routes.active_route,
     deliveries: state.deliveries
   }
 }
 
-export default connect(mapStateToProps, {getDeliveries, getRoutes})(RouteContainer);
+export default connect(mapStateToProps, {getDeliveries, getRoute})(RouteContainer);
