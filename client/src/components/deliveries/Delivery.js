e4bfe5469adfe8427257ceaefd4075c05785eb72
Menachem Hornbacher <mendelh1537@gmail.com>
2017-08-18 18:35:38 -0400

    Remove "Delivered at:" from before Not Delivered Yet

--- client/src/components/deliveries/Delivery.js
+++ client/src/components/deliveries/Delivery.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
 const Delivery = (props) => {
-  const delivered_at = props.delivered_at ? new Date(props.delivered_at).toLocaleString() : "Not Delivered Yet";
+  const delivered_at = props.delivered_at ? "Delivered at: " + new Date(props.delivered_at).toLocaleString() : "Not Delivered Yet";
   return (
     <div id={`delivery-${props.id}`} className={props.delivered ? "delivered" : "undelivered"}>
       <h2>Invoice number: {props.invoice_number}</h2>
       <h3>COD: ${props.cod}</h3>
-      <h3>Delivered at: {delivered_at}</h3>
+      <h3>{delivered_at}</h3>
 
       <h4>Tracking number: {props.tracking_number}</h4>
       {props.phone_number && <h4>Phone number: {props.phone_number}</h4>}
