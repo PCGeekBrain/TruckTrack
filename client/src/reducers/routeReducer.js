fa28fb95c2635975e433f2dc9024abc676d9ce57
Menachem Hornbacher <mendelh1537@gmail.com>
2017-08-18 16:13:25 -0400

    allow for setting the active route

--- client/src/reducers/routeReducer.js
+++ client/src/reducers/routeReducer.js
@@ -1,11 +1,15 @@
-export default (state = [], action) => {
+const initialState = {routes: [], active_route: undefined}
+
+export default (state = initialState, action) => {
   switch (action.type) {
     case "SET_NEW_ROUTES":
       if (Array.isArray(action.routes)){
-        return action.routes
+        return Object.assign({}, state, {routes: action.routes})
       } else {
         return state
       }
+    case "SET_ACTIVE_ROUTE":
+      return Object.assign({}, state, {active_route: action.route})
     default:
       return state
   }
