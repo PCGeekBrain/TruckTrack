1311142ba16240e734b55f7b5bc078c06d74948c
Menachem Hornbacher <mendelh1537@gmail.com>
2017-08-19 23:42:38 -0400

    added show and status actions

--- client/src/actions/route.js
+++ client/src/actions/route.js
@@ -15,6 +15,20 @@ const setActiveRoute = route => {
   }
 }
 
+export const setShowModal = show => {
+  return {
+    type: "SET_SHOW_ROUTE_MODAL",
+    show
+  }
+}
+
+const setStatusOptions = options => {
+  return {
+    type: "SET_STATUS_OPTIONS",
+    options
+  }
+}
+
 //** Async Actions */
 export const getRoutes = () => {
   return dispatch => {
@@ -27,5 +41,12 @@ export const getRoute = (id) => {
   return dispatch => {
     return API.get(`/routes/${id}`)
       .then(route => dispatch(setActiveRoute(route)))
+  }
+}
+
+export const getStatusOptions = () => {
+  return dispatch => {
+    return API.get(`/routes/options`)
+      .then(options => dispatch(setStatusOptions(options)))
   }
 }
