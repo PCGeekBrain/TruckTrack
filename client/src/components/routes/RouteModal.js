0bf7d91d44581ac46b7883d77cb25551f85368fd
Menachem Hornbacher <mendelh1537@gmail.com>
2017-08-20 22:55:21 -0400

    add it to the modal

--- client/src/components/routes/RouteModal.js
+++ client/src/components/routes/RouteModal.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 // Components
 import { Modal, Button, FormControl } from 'react-bootstrap';
 // Actions
-import { getStatusOptions, setShowModal } from '../../actions/route'
+import { getStatusOptions, setShowModal } from '../../actions/route';
+import { getDrivers } from '../../actions/driver';
 // Redux
 import { connect } from 'react-redux';
 
 class RouteModal extends Component {
   componentWillMount(){
-    this.props.getStatusOptions()
+    this.props.getStatusOptions();
+    this.props.getDrivers();
   }
 
   hide = () => {
@@ -18,6 +20,9 @@ class RouteModal extends Component {
   render(){
     const status_options = this.props.status_options.map((status, index) => {
       return <option key={index} value={status}>{status}</option>
+    });
+    const driver_options = this.props.driver_options.map((driver, index) => {
+      return <option key={index} value={driver.id}>{driver.username}</option>
     })
     return (
       <Modal show={this.props.showModal} onHide={this.hide}>
@@ -31,6 +36,7 @@ class RouteModal extends Component {
               {status_options}
             </FormControl>
             <FormControl componentClass="select" id="user_id">
+              {driver_options}
             </FormControl>
             <FormControl componentClass="select" id="truck_id">
             </FormControl>
@@ -47,8 +53,9 @@ class RouteModal extends Component {
 function mapStateToProps(state, ownProps) {
   return {
     status_options: state.routes.status_options,
-    showModal: state.routes.showModal
+    showModal: state.routes.showModal,
+    driver_options: state.drivers.drivers
   }
 }
 
-export default connect(mapStateToProps, { getStatusOptions, setShowModal })(RouteModal)
+export default connect(mapStateToProps, { getStatusOptions, setShowModal, getDrivers })(RouteModal)
