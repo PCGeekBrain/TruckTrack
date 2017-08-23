71c3f4da2ed68436318156df8d5a47fbcb4be347
Menachem Hornbacher <mendelh1537@gmail.com>
2017-08-22 21:42:21 -0400

    add edit button

--- client/src/components/routes/Route.js
+++ client/src/components/routes/Route.js
@@ -1,17 +1,25 @@
 import React from 'react';
 
+import { Button } from 'react-bootstrap';
+
 import '../../styles/routes/route.css';
 
-const Route = ({
-  id, log_number, status, delivery_count, 
-  updated_at, driver, truck
-}) => {
-  const last_updated = new Date(updated_at).toDateString();
+const Route = ({route, onEdit}) => {
+  const last_updated = new Date(route.updated_at).toDateString();
+  const edit = (event) => {
+    event.preventDefault();
+    event.stopPropagation();
+    onEdit(route)
+  }
   return (
     <div className="route-card card">
-      <h2>Log number: {log_number} - {delivery_count} Deliveries</h2>
-      <h4>Status: {status} - Last Updated: {last_updated}</h4>
-      <p>Driver: {driver.username} - Truck: {truck.name}</p>
+      <h2>Log number: {route.log_number} - {route.delivery_count} Deliveries</h2>
+      <h4>Status: {route.status} - Last Updated: {last_updated}</h4>
+      <p>
+        Driver: {route.driver ? route.driver.username : "N/A"} -
+        Truck: {route.truck ? route.truck.name : "N/A"}
+      </p>
+      <Button onClick={edit}>Edit</Button>
     </div>
   );
 }
