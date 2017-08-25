2308f5cc1742f5e431d627ce3fb633d6a113839a
Menachem Hornbacher <mendelh1537@gmail.com>
2017-08-25 15:49:17 -0400

    add some basic actions

--- client/src/actions/driver.js
+++ client/src/actions/driver.js
@@ -8,10 +8,46 @@ const setDrivers = drivers => {
   }
 }
 
+export const showModal = driver => {
+  return {
+    type: "SHOW_DRIVER_MODAL",
+    driver
+  }
+}
+
+export const hideModal = () => {
+  return {
+    type: "CLEAR_DRIVER_MODAL",
+  }
+}
+
 /** Async Actions */
 export const getDrivers = () => {
   return dispatch => {
     return API.get(`/users/drivers`)
       .then(drivers => dispatch(setDrivers(drivers)));
+  }
+}
+
+export const submitDriver = driver => {
+  return dispatch => {
+    // set the defaults
+    let url = "/users"
+    let request = API.post
+    // check if it is an update (we have an id)
+    if (driver.id) {
+      url += `/${driver.id}`;
+      request = API.patch;
+    }
+    // Push it up to the server
+    return request(url, driver)
+      .then(result => dispatch(getDrivers()))
+  }
+}
+
+export const deleteDriver = (id) => {
+  return dispatch => {
+    API.delete(`/users/${id}`)
+      .catch(() => dispatch(getDrivers())) // no content throws error since json is invalid
   }
 }
