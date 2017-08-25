4d32cfdfe5a6d8c8bd05601a5fd412ea8cb65612
Menachem Hornbacher <mendelh1537@gmail.com>
2017-08-25 15:51:07 -0400

    add the reducer changes

--- client/src/reducers/driverReducer.js
+++ client/src/reducers/driverReducer.js
@@ -1,11 +1,17 @@
 const initialState = {
-  drivers: []
+  drivers: [],
+  showModal: false,
+  driver: {}
 }
 
 export default (state = initialState, action) => {
   switch(action.type){
     case "SET_DRIVERS":
       return Object.assign({}, state, { drivers: action.drivers })
+    case "SHOW_DRIVER_MODAL":
+      return Object.assign({}, state, { showModal: true, driver: action.driver})
+    case "CLEAR_DRIVER_MODAL":
+      return Object.assign({}, state, { showModal: false, driver: {}})
     default:
       return state
   }
