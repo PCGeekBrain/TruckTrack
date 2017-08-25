7f3c3cc1e3646618cc6246812e5a55a499ad6937
Menachem Hornbacher <mendelh1537@gmail.com>
2017-08-25 15:43:56 -0400

    add driver container

---
+++ client/src/containers/dashboard/DriverContainer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class DriverContainer extends Component {
+  render(){
+    return (
+      <div className="page-drivers">
+        <h1 id="title">Drivers</h1>
+      </div>
+    )
+  }
+}
