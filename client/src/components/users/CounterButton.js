fb96c5607b00f7d9ef3c47c8da13541b207ee07a
Menachem Hornbacher <mendelh1537@gmail.com>
2017-10-07 20:17:54 -0400

    update to the increment function over internal state

--- client/src/components/users/CounterButton.js
+++ client/src/components/users/CounterButton.js
@@ -5,19 +5,30 @@ import { Button } from 'react-bootstrap';
 export default class CounterButton extends Component {
   componentWillMount() {
     this.setState({
-      count: 0
+      count: this.props.initial ? this.props.initial : 0
+    })
+  }
+
+  componentWillReceiveProps(nextProps){
+    this.setState({
+      count: nextProps.initial ? nextProps.initial : 0
     })
   }
 
   increment = () => {
-    this.setState({
-      count: this.state.count + 1
-    })
+    // if we where given a function to call, call it
+    if (this.props.onIncrement){
+      this.props.onIncrement();
+    } else {
+      debugger;
+    }
   }
 
   render(){
+    let buttonProps = Object.assign({}, this.props);
+    delete buttonProps.initial;
     return (
-      <Button {...this.props} onClick={this.increment}>
+      <Button {...buttonProps} onClick={this.increment}>
         {this.state.count}
       </Button>
     )
