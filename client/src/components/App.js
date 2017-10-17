056740445c49f6101506848c442061d7414fc4a2
Menachem Hornbacher <mendelh1537@gmail.com>
2017-10-17 00:12:15 -0400

    make 404 page actually show

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,20 +1,22 @@
 import React from 'react';
 // Components
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import HomeContainer from '../containers/HomeContainer';
 import LoginContainer from '../containers/LoginContainer';
 import DashboardContainer from '../containers/DashboardContainer';
+import NotFound from './NotFound';
 // Styles
 import './App.css';
 
 const App = () => {
   return (
     <Router>
-      <div>
+      <Switch>
         <Route exact path="/" component={HomeContainer} />
         <Route path="/login" component={LoginContainer} /> 
         <Route path="/dashboard" component={DashboardContainer} />
-      </div>
+        <Route component={NotFound} />
+      </Switch>
     </Router>
   );
 }
