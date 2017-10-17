844659f5be48580db0bc7ffd510c4011e6abe6e8
Menachem Hornbacher <mendelh1537@gmail.com>
2017-10-16 20:26:46 -0400

    Show the route number if provided

--- client/src/components/home/Delivery.js
+++ client/src/components/home/Delivery.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import '../../styles/home/Delivery.css';
 
-const Delivery = ({tracking_number, status}) => {
+const Delivery = ({tracking_number, status, route_number}) => {
   return (
     <li className="delivery-li">
       <div className="delivery-card">
         <h3>Status: {status}</h3>
-        <h4>Tracking Code: {tracking_number}</h4>
+        <h4>Tracking: {tracking_number}</h4>
+        {route_number && <h4>Route #{route_number}</h4>}
       </div>
     </li>
   );
